ad8465830be90afb4979404ed8c5330bc06bca02
Irving Hu <31359459+irvinghu07@users.noreply.github.com>
2018-12-02 00:48:37 -0800

    Implementing Hashtable with buckets of Array instead of Arraylist, put&get finished, working on other functions

--- src/Hashtable.java
+++ src/Hashtable.java
@@ -1,28 +1,33 @@
-import java.util.ArrayList;
-
 public class Hashtable implements HashTableSignature {
 
+    private static int DEFAULT_NUMBER_OF_BUCKETS = 2048;
     //    size of the table
     private int num_buckets;
 
     //    numebr of items in the Hashtable
     private int size;
 
-    private ArrayList<HashNode> buckets;
+    //    private ArrayList<HashNode> buckets;
+    private HashNode[] buckets;
 
     public Hashtable() {
-        this(0, 0);
+        this(DEFAULT_NUMBER_OF_BUCKETS);
     }
 
-    public Hashtable(int num_buckets, int size) {
+    public Hashtable(int num_buckets) {
         this.num_buckets = num_buckets;
-        this.size = size;
-        this.buckets = new ArrayList<>();
+        this.size = 0;
+        this.buckets = new HashNode[num_buckets];
+    }
+
+    private int getBucket(String key) {
+        return key.hashCode() % num_buckets;
     }
 
     public static void main(String[] args) {
         String a = "hello world";
-        String b = "hello Irving";
+        String b = "hello world";
+        System.out.println(a.equals(b));
         System.out.println(a.hashCode());
         System.out.println(b.hashCode());
     }
@@ -34,12 +39,37 @@ public class Hashtable implements HashTableSignature {
 
     @Override
     public String get(String key) {
+        int bucket_id = getBucket(key);
+        HashNode head = this.buckets[bucket_id];
+        if (null == head) {
+            return null;
+        } else {
+            while (null != head) {
+                if (key == head.getKey()) {
+                    return head.getValue();
+                }
+                head = head.getNext();
+            }
+        }
         return null;
     }
 
     @Override
     public void put(String key, String value) {
+        int bucket_id = getBucket(key);
+        HashNode node = buckets[bucket_id];
+        while (null != node) {
+            if (key == node.key) {
+                node.value = value;
+                return;
+            }
+            node = node.next;
+        }
 
+//        if reach this point, then the bucket is either empty or there is no match key related!! node == null now
+        HashNode newNode = new HashNode(key, value);
+        buckets[bucket_id] = newNode;
+        this.size++;
     }
 
     @Override
